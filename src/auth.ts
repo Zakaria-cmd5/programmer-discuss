a20498a6a82a3c434acab2de443136090731bbcb
Zakaria Asad <zasad2587@gmail.com>
2024-08-17 15:57:48 +0300

    Setup auth api file

--- src/auth.ts
+++ src/auth.ts
@@ -10,7 +10,7 @@ if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
   throw new Error("Missing Github Oauth Things");
 }
 
-const { handlers: {GET, POST}, auth, signOut, signIn } = NextAuth({
+export const { handlers: {GET, POST}, auth, signOut, signIn } = NextAuth({
   adapter: PrismaAdapter(db),
   providers: [
     GitHub({
