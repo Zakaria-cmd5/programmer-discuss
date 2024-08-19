a92cd5232c982a9618431bd3e3b6c4ba1d5af4cb
Zakaria Asad <zasad2587@gmail.com>
2024-08-19 21:10:15 +0300

    Build the Topic create form

--- src/app/topics/_components/TopicCreateForm.tsx
+++ src/app/topics/_components/TopicCreateForm.tsx
@@ -1,7 +1,35 @@
+import {
+  Button,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Textarea,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+
 const TopicCreateForm = () => {
   return (
-    <div>TopicCreateForm</div>
-  )
-}
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={actions.createTopic}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg font-bold">Create a Topic</h3>
+            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Textarea
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Descripe your topic"
+            />
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
 
-export default TopicCreateForm
+export default TopicCreateForm;
