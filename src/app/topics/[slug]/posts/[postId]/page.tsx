a23f0cf2bfab4ad415fb3ddaee7f7c329a2e8f8f
Zakaria Asad <zasad2587@gmail.com>
2024-08-29 14:32:40 +0300

    Add Post show component

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,25 @@
-const PostShowPage = () => {
-  return (
-    <div>PostShowPage</div>
-  )
+import paths from "@/paths";
+import Link from "next/link";
+import PostShow from "../_components/PostShow";
+
+interface Props {
+  params: {
+    slug: string;
+    postId: string;
+  };
 }
 
-export default PostShowPage
+const PostShowPage = ({ params }: Props) => {
+  const { slug, postId } = params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {"< "}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+    </div>
+  );
+};
+
+export default PostShowPage;
